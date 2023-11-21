8e840981234d6f739790e3ae6622f1428af571a5
Pureblackkk <247417351@qq.com>
2023-11-21 16:44:10 +0800

    feat: add mesh class

--- include/geometry.h
+++ include/geometry.h
@@ -1,26 +1,24 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
+#include<vector2.h>
 #include<vector3.h>
 #include<vector>
 
 class Geometry {
     public:
-        int nverts() const;
-        int nfaces() const;
-        Vector3f vert(const int i) const;
-        Vector3f vert(const int iface, const int nthvert) const;
-        Vector3f normal(const Vector2f &uv) const;
-        Vector3f normal(const int iface, const int nthvert) const;
-        Vector2f uv(const int iface, const int nthvert) const;
-
-    private:
         std::vector<Vector3f> verts{};
-        std::vector<Vector3f> tex_coord{};
+        std::vector<Vector2f> tex_coord{};
         std::vector<Vector3f> norms{};
         std::vector<int> facet_vrt{};
         std::vector<int> facet_tex{};
         std::vector<int> facet_nrm{};
+        int nverts() const;
+        int nfaces() const;
+        Vector3f vert(const int i) const;
+        Vector3f vert(const int iface, const int nthvert) const;
+        Vector3f normal(const int iface, const int nthvert) const;
+        Vector2f uv(const int iface, const int nthvert) const;
 };
 
 #endif
---
+++ src/geometry.cpp
@@ -0,0 +1,28 @@
+#include<geometry.h>
+
+int Geometry::nverts() const {
+    return verts.size();
+}
+
+int Geometry::nfaces() const {
+    return facet_vrt.size() / 3;
+}
+
+Vector3f Geometry::vert(const int i) const {
+    return verts[i];
+}
+
+Vector3f Geometry::vert(const int iface, const int nthvert) const {
+    return verts[facet_vrt[iface * 3 + nthvert]];
+}
+
+Vector3f Geometry::normal(const int iface, const int nthvert) const {
+    return norms[facet_nrm[iface * 3 + nthvert]];
+}
+
+Vector2f Geometry::uv(const int iface, const int nthvert) const {
+    return tex_coord[facet_tex[iface * 3 + nthvert]];
+}
+
+
+
